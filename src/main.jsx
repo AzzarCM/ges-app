8905e2a3bad1a10d31e19f011f7210844f0eb62e
Cristian Mundo <37192145+AzzarCM@users.noreply.github.com>
2023-11-16 22:18:35 -0600

    solved bug while scrolling to a # link (#4)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { GesApp } from './GesApp.jsx'
+import ScrollToAnchor from './ScrollToAnchor.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToAnchor/>
       <GesApp/>
     </BrowserRouter>
   </React.StrictMode>,
