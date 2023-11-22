b10b04d8cca634053d9da272af88b47ea71b64db
AzzarCM <cris.mundo@outlook.com>
2023-11-22 17:12:05 -0600

    correct padding values and banner is lighter

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -170,7 +170,7 @@ export const Navbar = () => {
                   )}
                 </Disclosure>
                 <NavLink
-                  to="/projects"
+                  to="/proyects"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   onClick={() => setMobileMenuOpen(false)}
                 >
